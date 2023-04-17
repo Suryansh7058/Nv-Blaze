d338e0e30bca8934416f8cb806775a4993840b20
SuryanshSingh <suryansh.singh1@pens.com>
2023-04-18 02:44:20 +0530

    Modified Highlighting for nvim-treesitter

--- lua/suryansh/plugins-setup.lua
+++ lua/suryansh/plugins-setup.lua
@@ -1,4 +1,4 @@
--- auto install packer if not installed
+-- auto install packer if not installedplugins
 local ensure_packer = function()
 	local fn = vim.fn
 	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
